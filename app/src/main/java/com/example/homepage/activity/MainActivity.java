4158f8429bd315fe0d1837bf9abdc0893301197c
thanhtuan76 <58723584+thanhtuan76@users.noreply.github.com>
2020-05-25 15:34:46 +0700

    main updated

--- app/src/main/java/com/example/homepage/activity/MainActivity.java
+++ app/src/main/java/com/example/homepage/activity/MainActivity.java
@@ -1,32 +1,105 @@
 package com.example.homepage.activity;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.transition.Slide;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
 import android.widget.ViewFlipper;
 
 import com.example.homepage.R;
 import com.google.android.material.navigation.NavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    Toolbar toolbar;
-    ViewFlipper viewFlipper;
-    RecyclerView recyclerView;
-    NavigationView navigationView;
-    ListView listView;
-    DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private ViewFlipper view_flipper;
+    private RecyclerView recyclerView;
+    private NavigationView navigationView;
+    private DrawerLayout drawer;
+
+    //View flipper
+    int[] imgs={
+            R.drawable.s1,
+            R.drawable.s2,
+            R.drawable.s3,
+            R.drawable.s4,
+            R.drawable.s5,
+            R.drawable.s6,
+            R.drawable.s7
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Initial();
+        setSupportActionBar(toolbar);
 
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
+        // Navigation bar
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        actionBar.setLogo(R.drawable.logo);
+        actionBar.setDisplayUseLogoEnabled(true);
+
+        actionBar.setTitle("");
+
+        //View flipper
+        for (int i = 0; i < imgs.length; i++){
+            flip_img(imgs[i]);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.action_bar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    //View flipper
+    public void flip_img(int i){
+        ImageView view = new ImageView(this);
+        view.setBackgroundResource(i);
+        view_flipper.addView(view);
+        view_flipper.setFlipInterval(4000);
+        view_flipper.setAutoStart(true);
+
+        view_flipper.setInAnimation(this, android.R.anim.slide_in_left);
+        view_flipper.setOutAnimation(this, android.R.anim.slide_out_right);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    private void Initial (){
+        toolbar = findViewById(R.id.toolbar);
+        view_flipper = findViewById(R.id.v_flipper);
+        recyclerView = findViewById(R.id.recycleViewMain);
+        navigationView = findViewById(R.id.nav_view);
+        drawer = findViewById(R.id.drawerLayout);
     }
 
 
