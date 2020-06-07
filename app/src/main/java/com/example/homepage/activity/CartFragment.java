a76f0e7cf8d5b16b4e439bc7668af9103d3247cb
thanhtuan76 <58723584+thanhtuan76@users.noreply.github.com>
2020-06-07 09:59:04 +0700

    Add anim for app

--- app/src/main/java/com/example/homepage/activity/CartFragment.java
+++ app/src/main/java/com/example/homepage/activity/CartFragment.java
@@ -23,6 +23,6 @@ public class CartFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cart_origin, container, false);
+        return inflater.inflate(R.layout.fragment_cart, container, false);
     }
 }
--- app/src/main/java/com/example/homepage/activity/InfoFragment.java
+++ app/src/main/java/com/example/homepage/activity/InfoFragment.java
@@ -23,6 +23,6 @@ public class InfoFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info_origin, container, false);
+        return inflater.inflate(R.layout.fragment_info, container, false);
     }
 }
