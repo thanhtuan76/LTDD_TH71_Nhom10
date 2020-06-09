fb04d4a0261baca4db2634f76c7d05d7b0e74dd7
HuyNguyen <huy.nq1711@gmail.com>
2020-06-10 06:00:14 +0700

    Add sign in activity

--- app/src/main/java/com/example/homepage/activity/InfoFragment.java
+++ app/src/main/java/com/example/homepage/activity/InfoFragment.java
@@ -1,5 +1,6 @@
 package com.example.homepage.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.homepage.R;
 
@@ -14,10 +16,22 @@ import com.example.homepage.R;
  * A simple {@link Fragment} subclass.
  */
 public class InfoFragment extends Fragment {
+    private Button btnSignIn, btnSignUp;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info, container, false);
+        View view = inflater.inflate(R.layout.fragment_info, container, false);
+        btnSignIn = view.findViewById(R.id.btnSignIn);
+        btnSignUp = view.findViewById(R.id.btnSignUp);
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), SigninActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
---
+++ app/src/main/java/com/example/homepage/activity/SigninActivity.java
@@ -0,0 +1,16 @@
+package com.example.homepage.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.homepage.R;
+
+public class SigninActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+}
